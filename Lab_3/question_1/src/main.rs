ee8e4e9e6ab24a480bec834a53b0e161c909d859
PhamNamSon <phamnamson1999@gmail.com>
2024-02-13 19:01:10 -0700

    Add Lab 3 files and implement search_member function

---
+++ Lab_3/question_1/src/main.rs
@@ -0,0 +1,13 @@
+fn search_member(groups: [[&str; 4]; 6], name: &str){
+
+}
+
+fn main(){
+    let mut groups = [[""; 4]; 6];
+    groups[0]=["Bob", "Carol", "Eric", "Matt"];
+    groups[1]=["Jim", "Lucy", "Terry", "Brenda"];
+    groups[2]=["Susan", "Brad", "Jim", "Matt"];
+    groups[3]=["Sue", "Wendy", "Sam", "Brad"];
+    groups[4]=["Kate", "Jack", "James", "Sydney"];
+    groups[5]=["Mary", "John", "Ricky", "Wendy"];
+}
